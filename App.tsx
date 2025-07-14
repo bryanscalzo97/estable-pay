e439c1713750d18656f3b813dacf48605ce1bd52
Bryan Scalzo <bryan@bantr.community>
2025-07-14 07:40:18 +0200

    chore: integrate React Query into App component and add API client setup

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
 import HomeScreen from './src/screens/HomeScreen';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './src/services/RQClientConfig';
 
-const App: React.FC = () => {
-  return <HomeScreen />;
-};
-
-export default App;
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HomeScreen />;
+    </QueryClientProvider>
+  );
+}
